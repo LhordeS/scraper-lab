efabdef6c32e5e9fe01d869fb984b67ca097d44a
Alain Mimeault <lhorde@msn.com>
2026-06-27 16:32:39 +0900

    Refactor server get request to dynamically accept bank from params. Add protective function that ensures the bank from the url param corresponds to bank in db

--- server/src/scrapers/index.ts
+++ server/src/scrapers/index.ts
@@ -4,11 +4,15 @@ import type { Scraper } from "./types.js";
 
 export const scrapers = {
   alphaBank: scrapeAlphaBank,
-  betaBank: scrapeBetaBank
+  betaBank: scrapeBetaBank,
 };
 
 export type ScraperName = keyof typeof scrapers;
 
 export function getScraper(name: ScraperName): Scraper {
   return scrapers[name];
+}
+
+export function isScraperName(name: string): name is ScraperName {
+  return name in scrapers;
 }
